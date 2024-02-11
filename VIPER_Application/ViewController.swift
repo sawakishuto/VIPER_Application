c1bdad108bfde71fbc317160faa7154c9ffb0daf
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-12 00:47:15 +0900

    viewControllerに少し追加

---
+++ VIPER_Application/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  VIPER_Application
+//
+//  Created by 澤木柊斗 on 2024/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var DeadLine: UILabel!
+    @IBOutlet weak var ToDotitle: UILabel!
+    @IBOutlet weak var ToDoTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
