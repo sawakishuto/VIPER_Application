e2d8ba2f8e3351bd8f523274c0e8f4d309a0c530
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-12 02:20:50 +0900

    viewController追加

--- VIPER_Application/ToDoList/View/ViewController.swift
+++ VIPER_Application/ToDoList/View/ViewController.swift
@@ -8,15 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+//    protocol越しにpresenterのメソッドを呼び出せる
+    var presenter: ToDoListPresenterProtocol?
+    var viewDatas = [TodoListViewData]() {
+        didSet {
+            self.ToDoTableView.reloadData()
+        }
+    }
 
-    @IBOutlet weak var DeadLine: UILabel!
-    @IBOutlet weak var ToDotitle: UILabel!
     @IBOutlet weak var ToDoTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.ToDoTableView.dataSource = self
+        self.ToDoTableView.delegate = self
+        print(self.viewDatas.count)
+
         // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.presenter?.viewWillApper()
     }
 
 
 }
+// ToDoListViewProtocolでのデータの設定
+extension ViewController: ToDoListViewProtocol {
+    func showToDos(_ todos: [TodoListViewData]) {
+        self.viewDatas = todos
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let todoId = self.viewDatas[indexPath.row].todoId
+        self.presenter?.didSelectRow(todoId)
+    }
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? TableListviewCell else {
+            print("Noting")
+            return UITableViewCell()
+        }
+        cell.titleLabel?.text = self.viewDatas[indexPath.row].title
+        cell.deadLineLabel.text = "\(self.viewDatas[indexPath.row].deadLine)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.viewDatas.count
+    }
+}
+
 
