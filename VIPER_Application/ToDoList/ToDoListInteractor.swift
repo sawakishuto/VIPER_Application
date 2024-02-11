91981cbd75a69ad6c964415ff4303d2cc7a8f10d
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-12 01:03:53 +0900

    用意したデータを追加

--- VIPER_Application/ToDoList/ToDoListInteractor.swift
+++ VIPER_Application/ToDoList/ToDoListInteractor.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+//ToDoListInteractorInputProtocol(InputProtocol　= 処理するやつ)で定義されたものを実装し、ToDoListInteractorOutputProtocol(OutputProtocol = presenterへ返すもの)のpresenterで値を移す
+final class ToDoListInteractor {
+//    値を移すためにpresenterを定義、OutputProtocolはpresenterを通して値を移すために定義
+    weak var presenter: ToDoListInteractorOutputProtocol?
+}
+extension ToDoListInteractor: ToDoListInteractorInputProtocol {
+    func fetchTodos() {
+       let todos = TodoStore.shared.todos
+        self.presenter?.didFetchedTodos(todos)
+    }
+    
+
+}
--- VIPER_Application/ToDoStore.swift
+++ VIPER_Application/ToDoStore.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class TodoStore {
+
+    static let shared = TodoStore()
+
+    private init() {}
+
+    var todos: [ToDoModel] = [
+        ToDoModel(id: 1, title: "カレーの具材を買う", detail: "カレールー・豚肉・人参・ジャガイモ、玉葱はあるのでいらない。", isCompleted: false, deadLine: Date(), createdOn: Date(), updateOn: Date()),
+        ToDoModel(id: 2, title: "歯医者の予約を取る", detail: "土曜日の11:00くらい", isCompleted: true, deadLine: Date(), createdOn: Date(), updateOn: Date()),
+        ToDoModel(id: 3, title: "友達に1000円返す", detail: "返さなくてもいいか", isCompleted: false, deadLine: Date(), createdOn: Date(), updateOn: Date()),
+        ToDoModel(id: 4, title: "ブログを100記事書く", detail: "あと65記事", isCompleted: false, deadLine: Date(), createdOn: Date(), updateOn: Date()),
+        ToDoModel(id: 5, title: "確定申告する", detail: "今年は4/16まで延期になった。", isCompleted: false, deadLine: Date(), createdOn: Date(), updateOn: Date())
+    ]
+}
