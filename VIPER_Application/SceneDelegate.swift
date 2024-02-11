3c1ab158cc3c36f29cf416cfa594e12a45c81199
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-12 02:43:24 +0900

    デリゲート変更

--- VIPER_Application/SceneDelegate.swift
+++ VIPER_Application/SceneDelegate.swift
@@ -17,6 +17,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: scene as! UIWindowScene)
+        self.window = window
+        window.makeKeyAndVisible()
+        let navigationController = UINavigationController()
+        let todoListViewController = TodoListRouter.assenbleModules()
+        navigationController.viewControllers = [todoListViewController]
+        window.rootViewController = navigationController
+    }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -48,5 +57,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
 
-}
+
 
