843427478000b18d21c3f0eb8aea4d81f4d51415
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-12 02:42:55 +0900

    routerの作成

--- VIPER_Application/ToDoList/Protocols/TodoListRouter.swift
+++ VIPER_Application/ToDoList/Protocols/TodoListRouter.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import UIKit
+
+final class TodoListRouter {
+    weak var view: ToDoListViewProtocol?
+}
+extension TodoListRouter: ToDoListRouterProtocol {
+    static func assenbleModules() -> UIViewController {
+        let view = ViewController.instantiate()
+        let presenter = TodoListPresenter()
+        let interactor = ToDoListInteractor()
+        let router = TodoListRouter()
+
+        view.presenter = presenter
+        presenter.view = view
+        presenter.interactor = interactor
+        presenter.router = router
+        interactor.presenter = presenter
+        router.view = view
+
+        return view
+    }
+    func trunsitionToDetailView(_ todoId: Int) {
+    }
+    
+
+}
