bfd84999a25e6585c100c4979bde2b93a61d4c19
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-12 10:28:12 +0900

    storybord変更と必要なファイル追加

---
+++ VIPER_Application/TodoDetail/TodoDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TodoDetailViewController.swift
+//  VIPER_Application
+//
+//  Created by 澤木柊斗 on 2024/02/12.
+//
+
+import Foundation
+import UIKit
+
+class TodoDetailViewController {
+    
+}
---
+++ VIPER_Application/TodoDetail/TodoDetailViewData.swift
@@ -0,0 +1,16 @@
+//
+//  TodoDetailViewData.swift
+//  VIPER_Application
+//
+//  Created by 澤木柊斗 on 2024/02/12.
+//
+
+import Foundation
+
+struct TodoDetailViewData {
+    let todoId: Int
+    let title: String
+    let detail: String
+    let isCompleted: Bool
+    let deadLine: Date
+}
