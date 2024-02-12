f9e155295e70145a224030e533c3c6db2ba1f939
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-12 12:18:44 +0900

    presenter実装

--- VIPER_Application/TodoDetail/TodoDetailPresenter.swift
+++ VIPER_Application/TodoDetail/TodoDetailPresenter.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+final class TodoDetailPresenter {
+    var view: TodoDetailViewProtocol?
+    var interactor: TodoDetailInteractorInput?
+    var router: TodoDetailRouterProtocol?
+    var todoId: Int = 0
+}
+extension TodoDetailPresenter: TodoDetailInteractorOutput {
+    func didFetchedTodo(_ todo: ToDoModel) {
+        var todoDetail: TodoDetailViewData = TodoDetailViewData(todoId: todo.id, title: todo.title, detail: todo.detail, isCompleted: todo.isCompleted, deadLine: todo.deadLine)
+        self.view?.showDetail(todoDetail)
+    }
+}
+extension TodoDetailPresenter: TodoDetailPresenterProtocol {
+    func viewWillApper() {
+        self.interactor?.fetchTodoDetail(self.todoId)
+    }
+}
