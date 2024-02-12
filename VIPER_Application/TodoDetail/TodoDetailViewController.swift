7dd3e1363addecc6647ddeed8a80606060fda8b1
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-12 12:19:43 +0900

    viewcontrollerに紐ずけ

--- VIPER_Application/TodoDetail/TodoDetailViewController.swift
+++ VIPER_Application/TodoDetail/TodoDetailViewController.swift
@@ -8,6 +8,22 @@
 import Foundation
 import UIKit
 
-class TodoDetailViewController {
+class TodoDetailViewController: UIViewController {
+
+    @IBOutlet weak var BackButton: UIBarButtonItem!
     
+    @IBAction func isNotCompleteButtonTapped(_ sender: UIButton) {
+    }
+    @IBOutlet weak var todoDetailTextView: UITextView!
+    @IBOutlet weak var endTimeLabel: UILabel!
+    @IBOutlet weak var deadLineLabel: UILabel!
+
+    @IBAction func isCompleteButtonTapped(_ sender: UIButton) {
+    }
+    
+
+    override func viewDidLoad() {
+
+    }
+
 }
