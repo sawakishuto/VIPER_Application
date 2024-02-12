960adaf16e0ef74d8643bdbd1bc650a47b1560ff
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-12 15:02:23 +0900

    これでuiviewcontrollerであることを知り（条件）pushviewcontrollerできる

--- VIPER_Application/TodoDetail/TodoDetailProtocols.swift
+++ VIPER_Application/TodoDetail/TodoDetailProtocols.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-protocol TodoDetailViewProtocol: AnyObject {
+protocol TodoDetailViewProtocol: TransitionProtocol {
     var presenter: TodoDetailPresenterProtocol? {get set}
     func showDetail(_ todo: TodoDetailViewData)
 }
