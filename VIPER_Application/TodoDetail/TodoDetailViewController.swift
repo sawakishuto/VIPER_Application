3da4480c4991c6d43d4165ea5e07b735f05c145b
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-12 15:02:58 +0900

    viewcontroller実装

--- VIPER_Application/TodoDetail/TodoDetailViewController.swift
+++ VIPER_Application/TodoDetail/TodoDetailViewController.swift
@@ -10,20 +10,46 @@ import UIKit
 
 class TodoDetailViewController: UIViewController {
 
-    @IBOutlet weak var BackButton: UIBarButtonItem!
-    
-    @IBAction func isNotCompleteButtonTapped(_ sender: UIButton) {
+    static func instantiate() -> TodoDetailViewController {
+        let storybord = UIStoryboard(name: "TodoDetailView", bundle: nil) 
+        let view = storybord.instantiateViewController(identifier: "TodoDetailView") as? TodoDetailViewController
+        return view ?? TodoDetailViewController()
     }
+
+    var viewData = TodoDetailViewData(todoId: 0, title: "", detail: "", isCompleted: false, deadLine: Date()) {
+        didSet {
+            self.detailNavigationTitle.title = viewData.title
+            self.deadLineLabel?.text = "\(viewData.deadLine)"
+            self.todoDetailTextView?.text = viewData.detail
+            self.isCompleteLabel.text = viewData.isCompleted ? "完了": "未完了"
+
+        }
+    }
+    var presenter: TodoDetailPresenterProtocol?
+    @IBOutlet weak var detailNavigationTitle: UINavigationItem!
+    @IBOutlet weak var DetailNavigationBar: UINavigationBar!
+    @IBOutlet weak var BackButton: UIBarButtonItem!
     @IBOutlet weak var todoDetailTextView: UITextView!
     @IBOutlet weak var endTimeLabel: UILabel!
     @IBOutlet weak var deadLineLabel: UILabel!
+    @IBAction func TappedBackView(_ sender: UIBarButtonItem) {}
+    @IBAction func isNotCompleteButtonTapped(_ sender: UIButton) {}
+    @IBAction func isCompleteButtonTapped(_ sender: UIButton) {}
+    @IBOutlet weak var isCompleteLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.presenter?.viewWillApper()
+    }
 
-    @IBAction func isCompleteButtonTapped(_ sender: UIButton) {
+}
+extension TodoDetailViewController: TodoDetailViewProtocol {
+
+    func showDetail(_ todo: TodoDetailViewData) {
+        self.viewData = todo
     }
     
-
-    override func viewDidLoad() {
-
-    }
 
 }
