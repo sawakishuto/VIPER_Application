da41316216dae644d3deb0dfb6f124a6e779ba2b
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-12 15:02:33 +0900

    router実装

--- VIPER_Application/TodoDetail/TodoDetailRouter.swift
+++ VIPER_Application/TodoDetail/TodoDetailRouter.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import UIKit
+
+final class TodoDetailRouter {
+//    上の層or下の層に伝播したいメソッドがある！
+    var view: ToDoListViewProtocol?
+}
+// このクラスに伝播が来て実施に実装する必要がある！
+extension TodoDetailRouter: TodoDetailRouterProtocol {
+    func assembleModules(_ todoId: Int) -> UIViewController {
+        let view = TodoDetailViewController.instantiate()
+        let presenter = TodoDetailPresenter()
+        let intaractor = TodoDetailInteractor()
+        let router = TodoDetailRouter()
+
+        view.presenter = presenter
+        presenter.interactor = intaractor
+        presenter.router = router
+        presenter.view = view
+        intaractor.presenter = presenter
+        router.view =  view as? any ToDoListViewProtocol
+
+        presenter.todoId = todoId
+        return view
+    }
+
+}
