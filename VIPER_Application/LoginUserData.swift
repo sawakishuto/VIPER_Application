ebb2bff9337ad16ab09db9de8ad7da5afef41461
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-13 11:38:56 +0900

    userDataを追加

---
+++ VIPER_Application/LoginUserData.swift
@@ -0,0 +1,14 @@
+//
+//  LoginUserData.swift
+//  VIPER_Application
+//
+//  Created by 澤木柊斗 on 2024/02/13.
+//
+
+import Foundation
+
+struct LoginUserData {
+    let id: Int
+    let userName: String
+    let password: Int
+}
