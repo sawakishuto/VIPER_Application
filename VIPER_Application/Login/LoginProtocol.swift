f708b872db8b1ee0f8c4ab7e13bc29f35c349076
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-13 11:39:42 +0900

    protocolを追加

--- VIPER_Application/Login/LoginProtocol.swift
+++ VIPER_Application/Login/LoginProtocol.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+protocol LoginViewControllerProtocol: TransitionProtocol {
+    var presenter: LoginPresenterProtocol? {get set}
+    func showLogin()
+}
+protocol LoginPresenterProtocol {
+    var view: LoginViewControllerProtocol? {get set}
+    var interactor: LoginInteractorInputProtocol? {get set}
+    var router: LoginRouterProtocol? {get set}
+    func startLoginComfirm(userName: String, password: Int)
+}
+protocol LoginInteractorInputProtocol {
+    var presenter: LoginInteractorOutputProtocol? {get set}
+    func comformLoginInformation(userName: String, password: Int)
+}
+protocol LoginInteractorOutputProtocol {
+    func ViewWillLoad()
+}
+protocol LoginRouterProtocol {
+    var view: LoginViewControllerProtocol? {get set}
+    func assembleModules()
+}
+
