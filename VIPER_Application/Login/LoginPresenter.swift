6b5acd5273e89b7a0c84cc16b1ac5688316ff75d
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-13 12:56:41 +0900

    presenter実装

--- VIPER_Application/Login/LoginPresenter.swift
+++ VIPER_Application/Login/LoginPresenter.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+
+final class LoginPresenter {
+    var view: LoginViewControllerProtocol?
+    var interactor: LoginInteractorInputProtocol?
+    var router: LoginRouterProtocol?
+}
+extension LoginPresenter: LoginPresenterProtocol{
+    func startLoginComfirm(userName: String, password: Int) {
+        self.interactor?.comformLoginInformation(userName: userName, password: password)
+    }
+}
+extension LoginPresenter: LoginInteractorOutputProtocol {
+    func failureConfirmedUserLoginInfomation(message: String) {
+        self.view?.failureUserLogin(message: message)
+    }
+    
+    func confirmedUserLoginInfomation(userData: LoginUserData) {
+        self.router?.transitionTodoList()
+    }
+}
+
