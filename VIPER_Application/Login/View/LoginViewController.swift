22daf709d1c86ee1ef519989f214c2443cc825f6
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-13 13:10:52 +0900

    ui紐付けとview修正

--- VIPER_Application/Login/View/LoginViewController.swift
+++ VIPER_Application/Login/View/LoginViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var userNameInputField: UITextField?
+    @IBOutlet weak var passwordInputField: UITextField?
+    @IBAction func isLoginStartButtonTapped(_ sender: UIButton) {}
+    @IBOutlet weak var loginErrorMessageLabel: UILabel?
     override func viewDidLoad() {
         super.viewDidLoad()
 
