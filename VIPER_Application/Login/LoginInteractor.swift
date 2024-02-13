ed0d4a5660c8a631ca9d581af62446f84d275b41
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-13 15:21:02 +0900

    ロジックの変更

--- VIPER_Application/Login/LoginInteractor.swift
+++ VIPER_Application/Login/LoginInteractor.swift
@@ -14,14 +14,15 @@ final class LoginInteractor {
 extension LoginInteractor: LoginInteractorInputProtocol {
     func comformLoginInformation(userName: String, password: Int) {
         let userDatas = ResistedUserDataStore.shared.LoginUser
-        for userData in userDatas {
-          if userData.userName == userName && userData.password == password {
-                self.presenter?.confirmedUserLoginInfomation(userData: userData)
-            }
-            else {
-                self.presenter?.failureConfirmedUserLoginInfomation(message: "ユーザーネームまたはパスワードが違います")
-            }
+        let comfirmResult = userDatas.contains { data in
+          let loginData = LoginUserData(userName: userName, password: password)
+          return loginData.userName == data.userName && loginData.password == data.password
+        }
+        if comfirmResult {
+            self.presenter?.confirmedUserLoginInfomation(userData: LoginUserData(userName: userName, password: password))
+        }
+        else {
+            self.presenter?.failureConfirmedUserLoginInfomation(message: "ユーザーネームまたはパスワードが違います")
         }
     }
 }
-
