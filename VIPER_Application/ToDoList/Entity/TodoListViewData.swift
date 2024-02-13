8a1d78358391e4e27817b6a6226d45f16a67277c
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-13 11:42:11 +0900

    戻した

---
+++ VIPER_Application/ToDoList/Entity/TodoListViewData.swift
@@ -0,0 +1,14 @@
+//
+//  TodoListViewData.swift
+//  VIPER_Application
+//
+//  Created by 澤木柊斗 on 2024/02/12.
+//
+
+import Foundation
+
+struct TodoListViewData {
+    let todoId: Int
+    let title: String
+    let deadLine: Date
+}
