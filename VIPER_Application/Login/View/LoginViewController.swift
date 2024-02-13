64974c65bb4bfa0e9dbf32950d6a0d84d0aa72e8
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-13 14:55:10 +0900

    viewのプレゼンテーションロジックを追加あ

--- VIPER_Application/Login/View/LoginViewController.swift
+++ VIPER_Application/Login/View/LoginViewController.swift
@@ -8,13 +8,41 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    @IBOutlet weak var userNameInputField: UITextField?
-    @IBOutlet weak var passwordInputField: UITextField?
-    @IBAction func isLoginStartButtonTapped(_ sender: UIButton) {}
-    @IBOutlet weak var loginErrorMessageLabel: UILabel?
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    static func instatiate() -> LoginViewController {
+        let storybord = UIStoryboard(name: "LoginView", bundle: nil)
+        let view =  storybord.instantiateViewController(identifier: "LoginViewController") as? LoginViewController
+        return view ?? LoginViewController()
 
     }
+    @IBOutlet weak var userNameInputField: UITextField?
+    @IBOutlet weak var passwordInputField: UITextField?
+    @IBAction func isLoginStartButtonTapped(_ sender: UIButton) {
+        guard let passwordText = passwordInputField?.text,
+                let password = Int(passwordText) else {
+            return
+          }
+        self.presenter?.startLoginComfirm(userName: userNameInputField?.text ?? "", password:  password )
+    }
+    @IBOutlet weak var loginErrorMessageLabel: UILabel?
+    var presenter: LoginPresenterProtocol?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+extension LoginViewController: LoginViewControllerProtocol {
+    func showLogin() {
+        print("show")
+    }
+    
+    func failureUserLogin(message: String) {
+        self.loginErrorMessageLabel?.text = message
+        self.loginErrorMessageLabel?.textColor = .red
+        self.userNameInputField?.backgroundColor = .lightGray
+        self.passwordInputField?.textColor = .lightGray
+        self.userNameInputField?.text = ""
+        self.passwordInputField?.text = ""
+    }
+    
 
 }
