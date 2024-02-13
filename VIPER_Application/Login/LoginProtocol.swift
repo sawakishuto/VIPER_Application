fba281ba6e139eb0c690b1857efdbdcd8924442c
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-13 11:11:54 +0900

    ログインのためのテンプレート追加

---
+++ VIPER_Application/Login/LoginProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  LoginProtocol.swift
+//  VIPER_Application
+//
+//  Created by 澤木柊斗 on 2024/02/13.
+//
+
+import Foundation
---
+++ VIPER_Application/Login/View/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  VIPER_Application
+//
+//  Created by 澤木柊斗 on 2024/02/13.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
