bbb23794f08a4c9045dfd22833f721363d27ba81
Sawaki Shuto <swkshuto0208@icloud.com>
2024-02-14 13:22:36 +0900

    tapした時の挙動変更

--- VIPER_Application/ToDoList/View/AddTodoButton.swift
+++ VIPER_Application/ToDoList/View/AddTodoButton.swift
@@ -7,14 +7,19 @@
 
 import UIKit
 
-class AddTodoButton: UIImageView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class AddTodoButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        let addGestureReco: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(TappedImageView))
+        addGestureRecognizer(addGestureReco)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    @objc func TappedImageView() {
+
+    }
 
 }
